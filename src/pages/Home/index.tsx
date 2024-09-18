0c42d51b2b8f0a0480fc973e849a6b346bb1ed4d
Meints <cadumeints0@gmail.com>
2024-09-18 17:20:52 -0300

    Finalizing the main page style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home(){
     return (
@@ -7,10 +7,27 @@ export function Home(){
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <input id="task" type="text" />
+                    <TaskInput 
+                        id="task" 
+                        list="task-suggestions"
+                        type="text" 
+                        placeholder="Dê um nome para o seu projeto" 
+                    />
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <input id="minutesAmount" type="number" />
+                    <MinutesAmountInput 
+                        id="minutesAmount" 
+                        type="number" 
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -23,10 +40,10 @@ export function Home(){
                     <span>0</span>
                 </CountdownContainer>
 
-                <button type="submit">
+                <StartCountdownButton disabled type="submit">
                     <Play size={24} />
                     Começar
-                </button>
+                </StartCountdownButton>
             </form>
         </HomeContainer>
     )
